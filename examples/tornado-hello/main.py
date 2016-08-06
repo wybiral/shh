a517dc966720164eb950cb80c894a7ccd5583fff
wybiral <davy.wybiral@gmail.com>
2016-08-06 18:02:14 -0500

    Added tornado server example

---
+++ examples/tornado-hello/main.py
@@ -0,0 +1,19 @@
+from tornado import ioloop, web
+from shh import HiddenService
+
+class MainHandler(web.RequestHandler):
+    def get(self):
+        self.write('Hello onion!')
+
+hidden = HiddenService()
+print hidden.onion
+
+app = web.Application([
+    (r"/", MainHandler),
+])
+
+app.listen(hidden.port)
+try:
+    ioloop.IOLoop.instance().start()
+except KeyboardInterrupt:
+    ioloop.IOLoop.instance().stop()
